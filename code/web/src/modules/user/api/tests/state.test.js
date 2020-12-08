7bf07977776ae7742061819794b3729d0bcec4f7
Eric Berglund <ericberglund117@gmail.com>
2020-12-08 16:53:39 -0700

    Pass first reducer test

--- code/web/src/modules/user/api/tests/state.test.js
+++ code/web/src/modules/user/api/tests/state.test.js
@@ -1,31 +1,42 @@
 import reducers from '../state.js';
 import React from 'react'
-import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import userEvent from '@testing-library/user-event'
-import Item from '../../../subscription/Item.js';
+
 
 describe('Reducers', () => {
-    const mockState = []
+    const mockDefaultState = {
+        error: null,
+        isLoading: false,
+        isAuthenticated: false,
+        details: null
+      }
 
     it('should return an array with a user object if the type is === SET_USER', () => {
+
+        const mockIsEmpty = jest.fn()
+        
         const mockSetUser = {
-            type: 'SET_USER',
-            isAuthenticated: true, 
+            type: 'AUTH/SET_USER',
+            user: {
+                 name: 'Hulk Hogan',
+                 email: 'Hulkster@yahoo.com',
+                 role: 'USER' 
+              }   
+            };
+
+        const expectedOutput = { 
             details: {
-                name: 'Hulk Hogan',
-                email: 'Hulkster@yahoo.com',
-                role: 'USER'
-            }
-        };
+              name: 'Hulk Hogan',
+              email: 'Hulkster@yahoo.com',
+              role: 'USER'
+            },
+            error: null,
+            isAuthenticated: true,
+            isLoading: false
+      }
 
-
-        const newUser = reducers(mockState, mockSetUser)
-
-        expect(newUser).toStrictEqual([{details: {
-            name: 'Hulk Hogan',
-            email: 'Hulkster@yahoo.com',
-            role: 'USER'
-        }}]);
+        const newUser = reducers(mockDefaultState, mockSetUser)
+    
+        expect(newUser).toEqual(expectedOutput)
     });
 })

c96c98be1a1b620edce93221c5e50da3de0d2579
Judith Pillado <judith.pillado@gmail.com>
2020-12-07 20:34:46 -0600

    Add notes for user/mutations

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -1,14 +1,25 @@
 // Imports
 import { GraphQLString, GraphQLInt } from 'graphql'
+  // This file writes all the mutation (post/patch/destroy) api calls for a user
+  // pulls graphql dataypes from graphql package
 
 // App Imports
 import { UserType } from './types'
+  // Imports UserType from the types file in the module/user/types.js file
+  // this method makes a GraphQL object while the model makes a database object/record
 import { create, remove } from './resolvers'
+  // retrieves the 'create' and 'remove' functions from the module/user/resolvers.js file
+  // Create: writes the behavior of making a create/post api call for Users
+  // Remove: writes a 'destroy' behavior if one makes an api call for Users by putting in ID
 
 // Create
 export const userSignup = {
+  // defines and exports userSignup const (create user) - mutation?
+  // defines what the user is allowed to send
   type: UserType,
+    // This is called from the UserType method in '.types.js' file (which makes it a graphql object)
   args: {
+    // Like a hash in Ruby
     name: {
       name: 'name',
       type: GraphQLString
@@ -25,6 +36,7 @@ export const userSignup = {
     }
   },
   resolve: create
+     // executes create function from 'modules/user/resolvers.js' file
 }
 
 // Remove

eb2338f6042fa91d0190f0cba876335f13292d0d
Judith Pillado <judith.pillado@gmail.com>
2020-12-07 20:42:49 -0600

    Add notes for user/query

--- code/api/src/modules/user/query.js
+++ code/api/src/modules/user/query.js
@@ -1,9 +1,21 @@
 // Imports
 import { GraphQLInt, GraphQLString, GraphQLList } from 'graphql'
+  // This file writes all the queries (get/show/new/index) api calls for a user
+  // pulls graphql dataypes from graphql package
 
 // App Imports
 import { UserType, UserLoginType, UserGenderType } from './types'
+  // Imports UserType, UserLoginType, and UserGenderType from the 'module/user/types.js' file.
+  // UserType makes a GraphQL object while the model makes a database object/record
+  // UserLoginType makes a GraphQL object for user authentication
+  // UserGenderType creates a GraphQL object for pulling by a particular gender
 import { getAll, getById, login, getGenders } from './resolvers'
+  // retrieves the 'getAll', 'getById', 'login', and 'getGenders' functions from the module/user/resolvers.js file
+  // getAll writes a behavior for calling all Users in the database
+  // getById writes a behavior for calling a single User by their id
+  // login passed params of email and password
+  // getGenders - writes a behavior for calling a list of user by a passed gender in the API call
+
 
 // All
 export const users = {

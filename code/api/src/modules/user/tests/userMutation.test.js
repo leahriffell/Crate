7e3968cd68af4dc987d5a8607459500b4577f7ea
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-12-09 05:50:39 -0700

    Update mutation test for user
    - The test it self will be easy to update, however, since one of our 
    current data attributes is null, resetting has turned into a new error: 
    How do we create a dummy user right here in the test and then destroy it
    - Currently solving a syntax error:
      - SyntaxError: Cannot use import statement outside a module

--- code/api/src/modules/user/tests/userMutation.test.js
+++ code/api/src/modules/user/tests/userMutation.test.js
@@ -1,14 +1,27 @@
+import models from '../../../setup/models'
+
 const chai = require('chai');
 
 const expect = chai.expect;
 const should = chai.should();
 const url = `http://localhost:8000/`;
 const request = require('supertest')(url);
+const User = require('../model');
 
 describe('User mutations', () => {
+  beforeEach(function() {
+
+  });
   afterEach(function() {
     request.post('/graphql')
-    .send({ query: 'mutation { userUpdate(id: 2, image: "https://en.pimg.jp/045/948/028/1/45948028.jpg", description: "Tattooed seitan waistcoat austin asymmetrical chambray hot chicken man bun poke") { id image description } }'})
+    // To ensure that the address line 2 comes back as null:
+        // Option 1: destroy user and recreate?
+        // Option 2: unseed user and reseed user on test database
+
+    .send({ query: 'mutation { userUpdate(id: 2, image: "https://en.pimg.jp/045/948/028/1/45948028.jpg", description: "Tattooed seitan waistcoat austin asymmetrical chambray hot chicken man bun poke", address_line1: "5678 Here Ave", address_line2: "", city: "Pueblo", state: "CO", zipcode: 85623 ) { id image description address_line1 address_line2 city state zipcode } }'})
+    .send({ query: 'mutation { userRemove(id: 2) { id } }'})
+    .send({ query: 'mutation { userSignup(name: "The User", email: "user@crate.com", password: "123456", image: "https://en.pimg.jp/045/948/028/1/45948028.jpg", description: "Tattooed seitan waistcoat austin asymmetrical chambray hot chicken man bun poke", address_line1: "5678 Here Ave", city: "Pueblo", state: "CO", zipcode: 85623 ) { name email password image description address_line1 address_line2 city state zipcode } }'})
+    .send({ query: 'mutation { userUpdate(id: 2) { id } }'})
     .then(() => {
       done();
     });
@@ -16,7 +29,7 @@ describe('User mutations', () => {
 
   it('Update attributes for user id = 2', (done) => {
     request.post('/graphql')
-    .send({ query: 'mutation { userUpdate(id: 2, image: "updated_img.png", description: "new me" ) { id image description } }'})
+    .send({ query: 'mutation { userUpdate(id: 2, image: "updated_img.png", description: "new me" ) { id image description address_line1 address_line2 city state zipcode } }'})
     .expect(200)
     .end((err,res) => {
         if (err) return done(err);

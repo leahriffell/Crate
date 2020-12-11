856554a06b29e3165602fe19e801e52c3dc302b1
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-12-11 08:11:26 -0700

    Update user query test for passing tests
    - All bugs have been solved
    - Sad path testing for various human errors (more to come)
    - Updates queries test for all 5 new columns for shipping address

--- code/api/src/modules/user/tests/userQuery.test.js
+++ code/api/src/modules/user/tests/userQuery.test.js
@@ -2,10 +2,8 @@
 const chai = require('chai');
 const expect = chai.expect;
 const should = chai.should();
-// Making requests in your test setup
-// const url = `http://localhost:8000/`;
+// Connecting to the server
 const request = require('supertest');
-// Adding in connection to database/graphql/server
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from '../../../setup/schema'
@@ -22,18 +20,21 @@ describe('User queries', () => {
         graphiql: false
       })
     );
+    console.log("INFO - Server started.");
   })
 
   after(async () => {
-    connection.close;
+    await connection.close;
+    console.log("INFO - Connection to server stopped.");
   })
 
   it('gets information for specific user', async () => {
     const user_request = await request(server)
       .post('/')
       .send({ query: '{ user(id: 2) { image email description address_line1 address_line2 city state zipcode } }'});
-    expect(user_request.statusCode).to.equal(200);
-    // console.log(user_request.body, "this is original request")
+
+    expect(user_request.statusCode).to.equal(200)
+
     user_request.body.data.user.should.have.property('image')
     expect(user_request.body.data.user.image).to.eq('https://en.pimg.jp/045/948/028/1/45948028.jpg')
 
@@ -56,44 +57,144 @@ describe('User queries', () => {
     expect(user_request.body.data.user.state).to.eq('CO')
 
     user_request.body.data.user.should.have.property('zipcode')
-    expect(user_request.body.data.user.zipcode).to.eq(85623)
+    expect(user_request.body.data.user.zipcode).to.eq(85623);
   });
 
+  it('attempts a get request for specific user without response request', async () => {
+    const bad_request = await request(server)
+      .post('/')
+      .send({ query: '{ user(id: 2) { } }' });
+    expect(bad_request.status).to.eq(400);
+    bad_request.body.should.have.property('errors')
+    bad_request.body.errors[0].should.have.property('message')
+    bad_request.body.errors[0].message.should.be.a('string');
+    expect(bad_request.body.errors[0].message).to.eq('Syntax Error: Expected Name, found }');
+  })
 
+  it('attempts a get request for specific user without any field', async () => {
+    const bad_request = await request(server)
+      .post('/')
+      .send({ query: '{ user(id: 2) }' });
+    expect(bad_request.status).to.eq(400);
+    bad_request.body.should.have.property('errors')
+    bad_request.body.errors[0].should.have.property('message')
+    bad_request.body.errors[0].message.should.be.a('string');
+    expect(bad_request.body.errors[0].message).to.eq('Field "user" of type "user" must have a selection of subfields. Did you mean "user { ... }"?');
+  })
 
-  it.skip('gets information for all users', async (done) => {
-    await request(server)
-    .post('/')
-    .send({ query: '{ users { image email description address_line1 address_line2 city state zipcode } }'})
-    .expect(200)
-    .end((err,res) => {
-        if (err) return done(err);
-        res.body.data.users.should.be.a('array')
-        res.body.data.users[0].should.have.property('image')
-        expect(res.body.data.users[0].image).to.eq('https://do.lolwot.com/wp-content/uploads/2015/06/18-hilarious-and-bizarre-stock-photos-15.jpg')
+  it('attempts a get request for specific user without incorrect field request (attribute does not exist)', async () => {
+    const bad_request = await request(server)
+      .post('/')
+      .send({ query: '{ user(id: 2) { tacopizzacat } }' });
+    expect(bad_request.status).to.eq(400);
+    bad_request.body.should.have.property('errors')
+    bad_request.body.errors[0].should.have.property('message')
+    bad_request.body.errors[0].message.should.be.a('string');
+    expect(bad_request.body.errors[0].message).to.eq('Cannot query field "tacopizzacat" on type "user".');
+  })
 
-        res.body.data.users[0].should.have.property('email')
-        expect(res.body.data.users[0].email).to.eq('admin@crate.com')
+  it('attempts a get request for specific user without specifying an id', async () => {
+    const bad_request = await request(server)
+      .post('/')
+      .send({ query: '{ user {email} }' });
+    expect(bad_request.status).to.eq(200);
+    bad_request.body.should.have.property('errors')
+    bad_request.body.errors[0].should.have.property('message')
+    bad_request.body.errors[0].message.should.be.a('string');
+    expect(bad_request.body.errors[0].message).to.eq('WHERE parameter "id" has invalid "undefined" value');
+  })
 
-        res.body.data.users[0].should.have.property('description')
-        expect(res.body.data.users[0].description).to.eq("I'm baby meggings church-key neutra, coloring book kitsch banh mi slow-carb pop-up irony snackwave")
+  // Should we be fixing the response to send a "no such user"? Right now, the getById resolver doesn't have an error pathway
+  it('attempts a get request for specific user with incorrect id', async () => {
+    const bad_request = await request(server)
+      .post('/')
+      .send({ query: '{ user(id: 200) { email } }' });
+    expect(bad_request.status).to.eq(200) //It is still a "successful" api request, it just returns nil body
 
-        res.body.data.users[0].should.have.property('address_line1')
-        expect(res.body.data.users[0].address_line1).to.eq('1234 There Blvd')
+    bad_request.body.data.should.have.property('user')
+    bad_request.body.data.should.not.have.nested.property('email')
+    expect(bad_request.body.data.user).to.eq(null);
+  })
 
-        res.body.data.users[0].should.have.property('address_line2')
-        expect(res.body.data.users[0].address_line2).to.eq('PO BOX 801234')
+  it('gets information for all users', async () => {
+    const all_users_request = await request(server)
+      .post('/')
+      .send({ query: '{ users { image email description address_line1 address_line2 city state zipcode } }'});
 
-        res.body.data.users[0].should.have.property('city')
-        expect(res.body.data.users[0].city).to.eq('Denver')
+    expect(all_users_request.statusCode).to.equal(200)
+    all_users_request.body.data.users.should.be.a('array')
 
-        res.body.data.users[0].should.have.property('state')
-        expect(res.body.data.users[0].state).to.eq('CO')
+    all_users_request.body.data.users[0].should.have.property('image')
+    expect(all_users_request.body.data.users[0].image).to.eq('https://do.lolwot.com/wp-content/uploads/2015/06/18-hilarious-and-bizarre-stock-photos-15.jpg')
 
-        res.body.data.users[0].should.have.property('zipcode')
-        expect(res.body.data.users[0].zipcode).to.eq(36479)
+    all_users_request.body.data.users[0].should.have.property('email')
+    expect(all_users_request.body.data.users[0].email).to.eq('admin@crate.com')
 
-        done();
-    })
+    all_users_request.body.data.users[0].should.have.property('description')
+    expect(all_users_request.body.data.users[0].description).to.eq("I'm baby meggings church-key neutra, coloring book kitsch banh mi slow-carb pop-up irony snackwave")
+
+    all_users_request.body.data.users[0].should.have.property('address_line1')
+    expect(all_users_request.body.data.users[0].address_line1).to.eq('1234 There Blvd')
+
+    all_users_request.body.data.users[0].should.have.property('address_line2')
+    expect(all_users_request.body.data.users[0].address_line2).to.eq('PO BOX 801234')
+
+    all_users_request.body.data.users[0].should.have.property('city')
+    expect(all_users_request.body.data.users[0].city).to.eq('Denver')
+
+    all_users_request.body.data.users[0].should.have.property('state')
+    expect(all_users_request.body.data.users[0].state).to.eq('CO')
+
+    all_users_request.body.data.users[0].should.have.property('zipcode')
+    expect(all_users_request.body.data.users[0].zipcode).to.eq(36479);
   });
+
+  it('attempts a get request for all users without response request', async () => {
+    const bad_request = await request(server)
+      .post('/')
+      .send({ query: '{ users { } }' });
+    expect(bad_request.status).to.eq(400);
+    bad_request.body.should.have.property('errors')
+    bad_request.body.errors[0].should.have.property('message')
+    bad_request.body.errors[0].message.should.be.a('string');
+    expect(bad_request.body.errors[0].message).to.eq('Syntax Error: Expected Name, found }');
+  })
+
+  it('attempts a get request for all users without any fields', async () => {
+    const bad_request = await request(server)
+      .post('/')
+      .send({ query: '{ users }' });
+    expect(bad_request.status).to.eq(400);
+    bad_request.body.should.have.property('errors')
+    bad_request.body.errors[0].should.have.property('message')
+    bad_request.body.errors[0].message.should.be.a('string');
+    expect(bad_request.body.errors[0].message).to.eq('Field "users" of type "[user]" must have a selection of subfields. Did you mean "users { ... }"?');
+  })
+
+  it('attempts a get request for all users with incorrect field request (attribute does not exist)', async () => {
+    const bad_request = await request(server)
+      .post('/')
+      .send({ query: '{ users { birthday } }' });
+    expect(bad_request.status).to.eq(400);
+    bad_request.body.should.have.property('errors')
+    bad_request.body.errors[0].should.have.property('message')
+    bad_request.body.errors[0].message.should.be.a('string');
+    expect(bad_request.body.errors[0].message).to.eq('Cannot query field "birthday" on type "user".');
+  })
+
+  // If you accidentally use 'user' instead of 'users'
+  it('attempts a get request for all users with incorrect query', async () => {
+    const bad_request = await request(server)
+      .post('/')
+      .send({ query: '{ user { email } }' });
+    expect(bad_request.status).to.eq(200) //It is still a "successful" api request, it just returns nil body
+
+    bad_request.body.errors.should.be.a('array')
+    bad_request.body.errors[0].message.should.be.a('string')
+
+    bad_request.body.data.should.not.have.property('users')
+    bad_request.body.data.should.not.have.nested.property('email')
+
+    expect(bad_request.body.data.user).to.eq(null);
+  })
 });
